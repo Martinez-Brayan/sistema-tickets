a6afb84bb6df2222a9ea8cd333e506d80e0da4e9
Brayan A <brayanmartinez1298@gmail.com>
2026-01-22 13:35:11 -0500

    /*-

--- src/componentes/calendario/Calendario.js
+++ src/componentes/calendario/Calendario.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FaChevronLeft, FaChevronRight, FaPlus, FaTimes, FaCalendarAlt, FaMapMarkerAlt,FaVideo,FaUser,FaClock } from 'react-icons/fa';
-import './Calendario.css';
+import './Calendario.css;
 
 function Calendario() {
   const [fechaActual, setFechaActual] = useState(new Date());
